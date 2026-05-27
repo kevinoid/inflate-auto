kevinoid/inflate-auto

commit e62bc2a0f523cd5c3a62ec3cf87650fba7ea4aaa

style: automatic fixes for new ESLint rules
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

index.js
@@ -834,7 +834,7 @@ InflateAuto.prototype._queueMethodCall = function _queueMethodCall(name, args) {
   // Ideally we would let the proxied method call the callback,
   // but callers may depend on a reply before the next write.
   // So call the callback now to avoid deadlocks.
-  const lastArg = args[args.length - 1];
+  const lastArg = args.at(-1);
   if (typeof lastArg === 'function') {
     args = Array.prototype.slice.call(args, 0, -1);
     queueMicrotask(lastArg);

test/inflate-auto.js
@@ -61,7 +61,7 @@ const SUPPORTED_FORMATS = [
       const invalid = Buffer.from(compressed);
       // gzip format has 4-byte CRC32 before 4-byte size at end
       // eslint-disable-next-line no-bitwise
-      invalid[invalid.length - 5] = invalid[invalid.length - 5] ^ 0x1;
+      invalid[invalid.length - 5] = invalid.at(-5) ^ 0x1;
       return invalid;
     },
     data: TEST_DATA,
@@ -150,7 +150,7 @@ const SUPPORTED_FORMATS = [
       const invalid = Buffer.from(compressed);
       // zlib format has 4-byte Adler-32 at end
       // eslint-disable-next-line no-bitwise
-      invalid[invalid.length - 1] = invalid[invalid.length - 1] ^ 0x1;
+      invalid[invalid.length - 1] = invalid.at(-1) ^ 0x1;
       return invalid;
     },
     data: TEST_DATA,

commit d1be4798c8da95b3b43077bc7f79c53ac4444056

fix: don't call #at() on Arguments
It causes

    TypeError: args.at is not a function

Fixes: e62bc2a0f523 ("style: automatic fixes for new ESLint rules")
Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

index.js
@@ -840,7 +840,9 @@ InflateAuto.prototype._queueMethodCall = function _queueMethodCall(name, args) {
   // Ideally we would let the proxied method call the callback,
   // but callers may depend on a reply before the next write.
   // So call the callback now to avoid deadlocks.
-  const lastArg = args.at(-1);
+  // (Note: Can't use .at() since Arguments doesn't define #at())
+  // eslint-disable-next-line unicorn/prefer-at
+  const lastArg = args[args.length - 1];
   if (typeof lastArg === 'function') {
     args = Array.prototype.slice.call(args, 0, -1);
     queueMicrotask(lastArg);