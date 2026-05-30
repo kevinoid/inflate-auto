697ef5efd22cc89a90d2b7a6bead2f1cac118161
Kevin Locke <kevin@kevinlocke.name>
2026-05-30 17:29:08 -0600

    fix: don't use non-existent Buffer#at()
    `Buffer` does not have an `at()` method like `Array`.
    
    Fixes: e62bc2a0f523 ("style: automatic fixes for new ESLint rules")
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- test/inflate-auto.js
+++ test/inflate-auto.js
@@ -57,8 +57,8 @@ const SUPPORTED_FORMATS = [
     corruptChecksum: function corruptGzipChecksum(compressed) {
       const invalid = Buffer.from(compressed);
       // gzip format has 4-byte CRC32 before 4-byte size at end
-      // eslint-disable-next-line no-bitwise
-      invalid[invalid.length - 5] = invalid.at(-5) ^ 0x1;
+      // eslint-disable-next-line no-bitwise,unicorn/prefer-at
+      invalid[invalid.length - 5] = invalid[invalid.length - 5] ^ 0x1;
       return invalid;
     },
     data: TEST_DATA,
@@ -146,8 +146,8 @@ const SUPPORTED_FORMATS = [
     corruptChecksum: function corruptZlibChecksum(compressed) {
       const invalid = Buffer.from(compressed);
       // zlib format has 4-byte Adler-32 at end
-      // eslint-disable-next-line no-bitwise
-      invalid[invalid.length - 1] = invalid.at(-1) ^ 0x1;
+      // eslint-disable-next-line no-bitwise,unicorn/prefer-at
+      invalid[invalid.length - 1] = invalid[invalid.length - 1] ^ 0x1;
       return invalid;
     },
     data: TEST_DATA,
